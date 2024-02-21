dc5234b9dfb02ccf983d7d89ebe02e97df64513a
Nila Enjeli <nilaanjeli1904@gmail.com>
2024-02-21 14:10:14 +0700

    basicjs

---
+++ Video/Vidio 16/lat.js
@@ -0,0 +1,11 @@
+alert('Selamat Datang di Orenda');
+var orenda = true;
+
+while(orenda === true){
+    var nama = prompt('masukkan nama:');
+    alert("Assalamualaikum "  + nama);
+    orenda = confirm('coba lagi');
+}
+    alert('Terima Kasih');
+
+
